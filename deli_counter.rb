1d7749c0a7a9882a3dad8089a57f30c4884c26da
Emily Wessel <ettl02@gmail.com>
2018-06-22 14:54:43 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,3 @@
-katz_deli = []
-
 def line(x)
   line_array = []
   if x.length == 0
@@ -8,18 +6,16 @@ def line(x)
     x.each.with_index(1) do |name, index|
       line_array.push("#{index}. #{name}")
     end
-    puts "This line is currently: #{line_array.join(" ")}"
+    puts "The line is currently: #{line_array.join(" ")}"
   end
 end
-
-def take_a_number (katz_deli, name)
-    katz_deli.push (name)
-    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
-
-def now_serving (array)
+def now_serving(array)
   if array.empty?
-   puts "There is nobody waiting to be served!"
+    puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{array[0]}."
     array.shift
