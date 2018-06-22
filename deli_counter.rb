f17d548fff272e2f6b7928e3c838af6b5d6d17fa
Emily Wessel <ettl02@gmail.com>
2018-06-22 14:36:41 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,6 +14,7 @@ def take_a_number (katz_deli, name)
     katz_deli.push (name)
     puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
   end
+  
 def now_serving (array)
   if array.empty?
     "There is nobody waiting to be served!"
