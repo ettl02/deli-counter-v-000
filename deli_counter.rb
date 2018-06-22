240c4ba258a9018f4e749f53bd3facbed1d0e410
Emily Wessel <ettl02@gmail.com>
2018-06-22 14:43:19 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,11 @@
 katz_deli = []
 
-def line (katz_deli)
+def line (x)
   line_array = []
-  if katz_deli.length == 0
+  if x.length == 0
     "The line is currently empty"
   else
-    katz_deli.each.with_index(1) do |name, index|
+    x.each.with_index(1) do |name, index|
       line_array.push("#{index}. #{name}")
     end
     puts "This line is currently: #{line_array.join(" ")}"
