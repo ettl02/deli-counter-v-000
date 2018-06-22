2b4ff8ce8fc780c71b3e8d0d209727c1b0cb65db
Emily Wessel <ettl02@gmail.com>
2018-06-22 13:33:34 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+def line (katz_deli)
+  line_array = []
+  if katz_deli.length == 0
+    "The line is currently empty"
+  else 
+    katz_deli.each.with_index(1)
+    return katz_deli
+
+def take_a_number (katz_deli, name)
+    katz_deli.push (name)
+    counter = 1
+    take_a_number.each {n}
+    counter += 1
+    input_to_index =
+      puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+
+def now_serving
+"There is nobody waiting to be served!"
